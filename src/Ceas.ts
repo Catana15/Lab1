f0ff10ba94f3877a593b6f47a0e231863654cfbe
Catana15 <124050247+Catana15@users.noreply.github.com>
2023-04-04 14:46:55 +0300

    Add files via upload

---
+++ src/Ceas.ts
@@ -0,0 +1,11 @@
+interface Ceas
+{
+    pret: number;
+    tipul: string;
+    marca: string;
+    carcasa: string;
+    in_stoc: number;
+
+}
+
+export default Ceas
---
+++ src/Rolex.ts
@@ -0,0 +1,10 @@
+import Ceas from "./Ceas";
+
+interface Rolecs extends Ceas
+{
+    modul_de_testare: string;
+    durata_producerii: number;
+
+}
+
+export default Rolecs;
